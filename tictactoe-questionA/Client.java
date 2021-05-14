861a236fafe7552c9c9cf358efb0f8ad272c0979
haxuanhuy <52565807+haxuanhuy@users.noreply.github.com>
2021-05-14 11:41:51 +0700

    Add files via upload

---
+++ tictactoe-questionA/Client.java
@@ -0,0 +1,154 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            String host = "localhost";
+            Registry registry = LocateRegistry.getRegistry(host);
+            ITicTacToe game = (ITicTacToe) registry.lookup("TicTacToe");
+            Scanner stdin = new Scanner(System.in);
+
+            // Register player on the remote server
+            int id = game.addPlayer(args[0]);
+            if (id == -1) {
+                System.err.println("Username already in use !");
+                System.exit(1);
+            }
+
+            if (id == -2) {
+                System.err.println("Maximum number of server players exceeded !");
+                System.exit(1);
+            }
+
+            int hasMatch = game.hasMatch(id);
+
+            System.out.println("Looking for match  ...");
+
+            // Checks if there are any active matches
+            while (hasMatch != 1 && hasMatch != 2) {
+                if (hasMatch == -2) {
+                    System.err.println("Timeout !");
+                    System.exit(1);
+                }
+
+                if (hasMatch == -1) {
+                        System.err.println("Server error !");
+                    System.exit(1);
+                }
+
+                // Check every 1 second
+                Thread.sleep(1000);
+                hasMatch = game.hasMatch(id);
+            }
+
+                System.out.println("Second player  " + game.getOpponent(id) + " came in ....");
+
+            int isMyTurn;
+            String message = null;
+
+            // Game Loop
+            while (true) {
+
+                // Checks if it's that player's turn
+                isMyTurn = game.isMyTurn(id);
+
+                if (isMyTurn == -2) {
+                    System.err.println("There are no two players in this match !");
+                    game.endMatch(id);
+                    System.exit(1);
+                }
+
+                if (isMyTurn == -1) {
+                    System.err.println("Server error !");
+                    game.endMatch(id);
+                    System.exit(1);
+                }
+
+                switch (isMyTurn) {
+                    case 2:
+                            message = "You won!";
+                        break;
+                    case 3:
+                        message = "You lost!";
+                        break;
+                    case 4:
+                        message = "Tie!";
+                        break;
+                    case 5:
+                        message = "You won by WO !";
+                        break;
+                    case 6:
+                        message = "You lost by WO !";
+                        break;
+                }
+
+                // End of game, displays result and closes
+                if (isMyTurn > 1 && isMyTurn < 7) {
+                    System.out.println(message);
+
+                    if (game.endMatch(id) == -1) {
+                        System.err.println("Error when ending game !");
+                        System.exit(1);
+                    } else {
+                        System.out.println("Game over !");
+                        System.exit(0);
+                    }
+                }
+
+                // You can only move the pieces on the board
+                int ret_movePeca = -1;
+
+                while ((ret_movePeca != 1) && (ret_movePeca != -3) && (isMyTurn == 1)) {
+
+                    System.out.println(game.getBoard(id));
+
+                    System.out.println("Enter the position of the part to be moved. ");
+                    System.out.print("Move : ");
+                    int row= stdin.nextInt();
+
+                    System.out.print("Column: ");
+                    int column = stdin.nextInt();
+
+                    ret_movePeca = game.move(id, row, column);
+
+                    switch (ret_movePeca) {
+                        case 2:
+                            System.out.println("You lost by WO !");
+                            game.endMatch(id);
+                            System.exit(0);
+                        case 1:
+                            System.out.println("Successfully completed play ");
+                            System.out.println(game.getBoard(id));
+                            break;
+                        case 0:
+                            System.out.println("Invalid position !");
+                            break;
+                        case -1:
+                            System.out.println("Invalid parameters !");
+                            break;
+                        case -2:
+                            System.err.println("There are no two players in this match !");
+                            game.endMatch(id);
+                            System.exit(1);
+                        case -3:
+                            System.out.println("Invalid parameters !");
+                            break;
+                        case -4:
+                            System.out.println("It's not your turn to move !");
+                            break;
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println("TicTacToe client failed!");
+            System.err.println(e.toString());
+        }
+    }
+}
